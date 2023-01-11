4a2748b3e8df2dc6b91ee304303719f6c6985b16
Damian Tarira <damiantarira@ceroca.cat>
2023-01-11 20:26:44 +0100

    FRAGMENT - PENTAKILL Carregant fragements

--- app/src/main/java/com/example/fragment_mvm/MainActivity.java
+++ app/src/main/java/com/example/fragment_mvm/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.fragment_mvm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //PASO 1
+    private Button mBtnCarregarFragment1, mBtnCarregarFragment2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //PASO 2
+        mBtnCarregarFragment1 = findViewById(R.id.BTN_Fragment1);
+        mBtnCarregarFragment2 = findViewById(R.id.BTN_Fragment2);
+
+
+        //Paso 3
+        mBtnCarregarFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Carregem el fragment 1
+
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContingutFragments, new Fragment1()).commit();
+
+            }
+
+        });
+        mBtnCarregarFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContingutFragments, new Fragment2()).commit();
+            }
+        });
     }
+
+
 }
